9f1e37096c2ae378b5863dd8fac346d5ff5c6572
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2016-01-19 15:44:29 -0800

    Screwing around with comments

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './components/Navbar';
+import Comment from './components/Comment';
 
 ReactDOM.render(<Navbar />, document.getElementById('navbar'));
+ReactDOM.render(<Comment />, document.getElementById('test'));
---
+++ js/components/Comment.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import RegisterForm from './RegisterForm';
+import API from '../API';
+
+class Comment extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is not art</h1>
+      </div>
+    )
+  }
+}
+
+export default Comment;
