d66d90434b119fa7025661e387dd30251aff5df7
paulgoblin <mpr75@cornell.edu>
2016-01-22 12:08:25 -0800

    add store Class in store.js

--- js/util/store.js
+++ js/util/store.js
@@ -1,9 +1,9 @@
 import API from '../API'
 
 let callbacks = Symbol();
+let data = Symbol();
 
 class EventEmitter {
-
   constructor() {
     this[callbacks] = {};
   }
@@ -14,7 +14,19 @@ class EventEmitter {
   emitChange(name) {
     this[callbacks][name].forEach( cb => cb() );
   }
+}
 
+class Store extends EventEmitter {
+  constructor() {
+    super();
+    this[data] = {};
+  }
+  saveDatum(name, datum) {
+    this[data][name] = datum;
+  }
+  getDatum(name) {
+    return this[data][name];
+  }
 }
 
 export const eventEmitter = new EventEmitter();
