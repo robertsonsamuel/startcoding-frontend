930a103853d4c52c4af307468d7d18d6456eb09f
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2016-01-25 22:55:48 -0800

    grandma urls not working

--- js/app.jsx
+++ js/app.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, Link, IndexLink, browserHistory } from 'react-router'
+
 import Navbar from './components/Navbar.jsx';
-import Main from './components/Main.jsx';
+// import Main from './components/Main.jsx';
 import '../css/sweetalert.css';
 import '../css/google.css';
 import '../css/style.css';
@@ -10,13 +12,61 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Navbar />
         <div className="container">
-          <Main />
+          {this.props.children}
         </div>
       </div>
     )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('React'));
+
+class Landing extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Landing Page</h1>
+        <ul>
+          <li><Link to="/all">All</Link></li>
+          <li><Link to="/js">JS</Link></li>
+          <li><Link to="/html">HTML</Link></li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.params.category}</h1>
+        <ul>
+          <li><Link to="/">Landing Page</Link></li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+
+
+
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+
+      <IndexRoute component={Landing}/>
+    
+      <Route path=":category" component={Main}/>
+    
+    </Route>
+  </Router>
+), document.getElementById('React'))
+
+
+      // <Route path="users" component={Users}>
+      //   <IndexRoute component={UsersIndex}/>
+      //   <Route path=":id" component={User}/>
+      // </Route>
