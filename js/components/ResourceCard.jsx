8e79eee82f12223f3962f3e260dec3e30b629e5b
paulgoblin <mpr75@cornell.edu>
2016-01-30 18:22:08 -0800

    fix voteBtn color bug

--- js/components/ResourceCard.jsx
+++ js/components/ResourceCard.jsx
@@ -31,6 +31,7 @@ class Resource extends React.Component {
   }
 
   handleVote(vote) {
+    console.log("voting", vote, this.state.votingInProcess);
     if (this.state.votingInProcess) return;
 
     this.setState({ votingInProcess: true });
