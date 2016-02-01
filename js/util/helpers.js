e1c0f714a38138261948f1a8fc4cd938a292d3d7
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2016-01-31 20:09:00 -0800

    Add comments describing what the time function does

--- js/util/helpers.js
+++ js/util/helpers.js
@@ -4,7 +4,7 @@ export function formatTime(timestamp) {
   if (!timestamp) return 'deleted';
 
   let diff = moment().diff(moment(timestamp));
-  if (diff <= 0) return 'just now';
-  if (diff > 31536000000) return moment(timestamp).format('ll');
+  if (diff <= 0) return 'just now'; // if the timestamp appears to be in the future
+  if (diff > 31536000000) return moment(timestamp).format('ll'); // if older than a year
   return moment(timestamp).fromNow();;
 }
